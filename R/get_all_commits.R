345d1b793a98437480630f2509b1d3ed02182db3
Phil Chapman <chapmandu2@gmail.com>
2019-02-08 10:00:20 +0000

    get commit diff correct

--- R/get_all_commits.R
+++ R/get_all_commits.R
@@ -9,7 +9,7 @@
 #' NULL
 get_all_commits <- function(git_repo) {
 
-  purrr::map(2:length(git2r::commits(git_repo)), ~get_single_commit(git_repo, .)) %>%
+  purrr::map(1:(length(git2r::commits(git_repo))-1), ~get_single_commit(git_repo, .)) %>%
     dplyr::bind_rows()
 
 }
--- R/get_single_commit.R
+++ R/get_single_commit.R
@@ -12,12 +12,12 @@
 get_single_commit <- function(git_repo, commit_id = 10) {
 
   this_commit <- git2r::commits(git_repo)[[commit_id]]
-  previous_commit <- git2r::commits(git_repo)[[commit_id - 1]]
+  previous_commit <- git2r::commits(git_repo)[[commit_id + 1]]
 
   tree_1 <- git2r::tree(this_commit)
   tree_2 <- git2r::tree(previous_commit)
 
-  code_df <- dplyr::data_frame(code=git2r::diff(tree_1, tree_2, as_char = TRUE) %>%
+  code_df <- dplyr::data_frame(code=git2r::diff(tree_2, tree_1, as_char = TRUE) %>%
                           base::strsplit('\n')) %>%
     tidyr::unnest() %>%
     dplyr::filter(grepl('^\\+', code)) %>%
@@ -28,6 +28,7 @@ get_single_commit <- function(git_repo, commit_id = 10) {
   dplyr::data_frame(sha = this_commit$sha,
              author_name = this_commit$author$name,
              author_email = this_commit$author$email,
+             commit_summary = this_commit$summary,
              commit_time = lubridate::as_datetime(as.character(this_commit$author$when)),
              code = all_code) %>%
     dplyr::mutate(code_length = base::nchar(code)) %>%
